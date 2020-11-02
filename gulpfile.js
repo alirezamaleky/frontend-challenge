5d0b19b1c578f784c23163375ba90973fe0cbc2a
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-11-02 12:00:37 +0330

    configure gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const { src, dest } = require("gulp");
+const babel = require("gulp-babel");
+const browserify = require("gulp-browserify");
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+
+exports.default = () => {
+  src("src/css/index.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(dest("dist"));
+
+  return src("src/js/index.js")
+    .pipe(browserify({ insertGlobals: true }))
+    .pipe(babel({ presets: ["@babel/env"] }))
+    .pipe(uglify())
+    .pipe(rename({ extname: ".min.js" }))
+    .pipe(dest("dist/"));
+};
