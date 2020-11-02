e8b6371c0baeff09b7cf56ce833443bb1853174b
Ali Abdolmaleki <a.abdolmaleky@kianiranian.com>
2020-11-02 19:02:21 +0330

    login layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
 const ko = require("knockout");
 const createUser = require("sdk");
 
-const ViewModel = function (first, last) {
-  this.firstName = ko.observable(first);
-  this.lastName = ko.observable(last);
+class ViewModel {
+  constructor() {
+    this.name = ko.observable("");
+    this.email = ko.observable("");
+    this.age = ko.observable("");
+    this.step = ko.observable(0);
+    this.submitForm = function (form) {
+      const name = form.elements["name"].value;
+      const email = form.elements["email"].value;
+      const age = form.elements["age"].value;
+      if (![name.length, email.length, age.length].includes(0)) {
+        createUser({ name, email, age }).then((res) => {
+          console.log(res);
+        });
+        this.step = ko.observable(1);
+      }
+    };
+  }
+}
 
-  this.fullName = ko.pureComputed(function () {
-    return this.firstName() + " " + this.lastName();
-  }, this);
-};
-
-ko.applyBindings(new ViewModel("", ""));
+ko.applyBindings(new ViewModel());
