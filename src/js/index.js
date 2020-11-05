325ea7d59f17879f0c5402641a7716ec3e2cfc2d
Alireza Maleky <alirezaabdalmaleky@gmail.com>
2020-11-05 11:01:42 +0330

    subscribe step

--- src/js/index.js
+++ src/js/index.js
@@ -6,19 +6,27 @@ class ViewModel {
     this.name = ko.observable("");
     this.email = ko.observable("");
     this.age = ko.observable("");
-    this.step = ko.observable(0);
-    this.submitForm = function (form) {
-      const name = form.elements["name"].value;
-      const email = form.elements["email"].value;
-      const age = form.elements["age"].value;
-      if (![name.length, email.length, age.length].includes(0)) {
-        createUser({ name, email, age }).then((res) => {
-          console.log(res);
-        });
-        this.step = ko.observable(1);
-      }
-    };
+    this.step = ko.observable(1);
+    this.subscribe = ko.observable(false);
+    this.submitForm = function () {};
   }
 }
 
-ko.applyBindings(new ViewModel());
+const vm = new ViewModel();
+ko.applyBindings(vm);
+vm.submitForm = function (form) {
+  const name = form.elements["name"] ? form.elements["name"].value : "";
+  const email = form.elements["email"] ? form.elements["email"].value : "";
+  const age = form.elements["age"] ? form.elements["age"].value : "";
+  const subscribe = form.elements["subscribe"]
+    ? form.elements["subscribe"].value === "on"
+    : false;
+  if (![name.length, email.length, age.length].includes(0)) {
+    createUser({ name, email, age }).then((res) => {
+      vm.step(2);
+    });
+  }
+  if (subscribe) {
+    console.log("subscribed");
+  }
+};
